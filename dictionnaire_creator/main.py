7d633160340aab17eb18e4bf7c7ac1544e882540
alliance24 <70445622+alliance24@users.noreply.github.com>
2022-11-04 12:32:21 +0100

    Add files via upload

--- dictionnaire_creator/main.py
+++ dictionnaire_creator/main.py
@@ -4,16 +4,19 @@ import string
 CARACTERE_min = list(string.ascii_lowercase)
 CARACTERE_maj = list(string.ascii_uppercase)
 NOMBRES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-général = (CARACTERE_maj + CARACTERE_min + NOMBRES)
-
-def main(cb_caractères):
-    file = open("dictionnaire_creator\\dictionnaire.txt", "a+")
-    
-    
-    file.close
+value = (CARACTERE_maj + CARACTERE_min + NOMBRES)
 
 
+def create_dico(n):
+    o = 0
+    final = ""
+    for i in range(n**len(value)):
+        for j in range(len(value)):
+            final += value[j]
+            o+=1
+        print(final)
 
 
+        
 
-main()
+create_dico(1)
