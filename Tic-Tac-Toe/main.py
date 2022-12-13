1c3a420e48b12def7f5e5c2794f28553f3460ffc
alliance24 <70445622+alliance24@users.noreply.github.com>
2022-12-13 15:58:01 +0100

    Add files via upload

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -18,11 +18,81 @@ def affiche(g):
                 print("   O", end='')
         print("\n")
 
-def coup_possible(g, c): # c doit etre l'index
-    if c >3 or c <0:
+def coup_possible(g, l, c): 
+    if g[l][c] == 0:
+        return True
+    else: 
         return False
-    if g[0][c] == 0: return True
-    else: return False
+    
+def jouer(g, j, l, c):
+    g[l][c]=j
+    
+def horiz(g,j,l,c):
+    i=2
+    while c-i<0:
+        i-=1
+    while i>=0 and c-i<=3:
+        if g[l][c-i]==j and g[l][c-i+1]==j and g[l][c-i+2]==j:
+            return True
+        i-=1
+    return False
 
-g = grille_vide()
-affiche(g)
+def vert(g,j,l,c):
+    i=2
+    while l-i<0:
+        i-=1
+    while i>=0 and l-i<=2:
+        if g[l-i][c]==j and g[l-i+1][c]==j and g[l-i+2][c]==j:
+            return True
+        i-=1
+    return False
+
+def diag(g,j,l,c):
+    i=2
+    while c-i<0 or l-i<0: #Diagonale SO-NE
+        i-=1
+    while i>=0 and c-i<=3 and l-i<=2:
+        if g[l-i][c-i]==j and g[l-i+1][c-i+1]==j and g[l-i+2][c-i+2]==j:
+            return True
+        i-=1
+    i=2
+    while c-i<0 or l+i>5: #Diagonale NO-SE
+        i-=1
+    while i>=0 and c-i<=3 and l+i>=3:
+        if g[l+i][c-i]==j and g[l+i-1][c-i+1]==j and g[l+i-2][c-i+2]==j:
+            return True
+        i-=1
+    return False
+
+def victoire(g,j):
+    for a in range(3):
+        for b in range(3):
+            if horiz(g,j,a,b) or vert(g,j,a,b) or diag(g,j,a,b):
+                return True
+    return False
+
+def match_nul(g):
+    for l in range(3):
+        for c in range(3):
+            if coup_possible(g,c):
+                return False
+    return True
+
+def coup_aleatoire(g,j):
+    k=False
+    while k==False:
+        c=randint(0,6)
+        k=coup_possible(g,c)
+    return jouer(g,j,c)
+
+def main():
+    g = grille_vide()
+    bot = 0
+    joueur = 1
+    affiche(g)
+    if randint(0, 1) == 0:
+        coup_aleatoire(g, bot)
+        
+        
+    
+main()
